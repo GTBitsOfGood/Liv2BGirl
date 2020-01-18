b4297f1768116ba275853cbe1943b559bb0fdf77
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-01-18 02:18:42 -0500

    Added MongoDB server action examples

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ExampleText from "../client/components/ExampleText";
 
 const HomePage = () => (
-  <div>
+  <>
     <ExampleText>Welcome to Next.js!</ExampleText>
-  </div>
+  </>
 );
 
 export default HomePage;
