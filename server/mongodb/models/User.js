5a1c1f1a5e0d057592e049b4fc1fa5a84c07c2a8
Xingyun Chang <xchang31@gatech.edu>
2020-02-04 23:46:09 -0500

    Update User.js

--- server/mongodb/models/User.js
+++ server/mongodb/models/User.js
@@ -16,6 +16,13 @@ const UserSchema = new Schema({
   isAdmin: {
     type: Boolean,
     default: false
+  },
+  following: {
+    type: [String],
+  },
+  followers: {
+    type: [String],
+    
   }
 });
 
