356aae50abf43f9d24fdf7ed0cb7a3163c7b64f0
Vibhav Bhat <vibhavb00@gmail.com>
2020-02-11 16:27:26 -0500

    merged master in

---
+++ pages/profile/[userid].jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const ProfilePage = () => {
+  const router = useRouter();
+  const { userid } = router.query;
+
+  return (
+    <p>
+      User:
+      {userid}
+    </p>
+  );
+};
+
+export default ProfilePage;
