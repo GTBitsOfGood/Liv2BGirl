8eaf69e727428840db22b875f7c234746761f732
Vibhav Bhat <vibhavb00@gmail.com>
2020-02-11 17:23:01 -0500

    Fixed typos

--- pages/profile/[userid].jsx
+++ pages/profile/[userid].jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-import ProfilePage from '../../frontend/containers/Profile/ProfilePage';
-
-
+import ProfilePage from "../../frontend/containers/Profile/ProfilePage";
 
 const Profile = () => {
   const router = useRouter();
   const { userid } = router.query;
 
-  return <ProfilePage userid={ userid } />;
-  
+  return <ProfilePage userid={userid} />;
 };
 
 export default Profile;
