4b2d675dff51b15613bbbe6c1e20bfaf9e1e8ef2
Marto37 <mcosta33@gatech.edu>
2020-02-11 19:18:31 -0500

    deleted .env

--- pages/api/signUp.js
+++ pages/api/signUp.js
@@ -4,20 +4,25 @@ import { signUp } from "../../server/mongodb/actions/User";
 // @desc    SignUp Request
 // @access  Public
 
-
 const handler = (req, res) =>
-  signUp(req.body.username, req.body.password, req.body.email, req.body.role, req.body.name)
+  signUp(
+    req.body.username,
+    req.body.password,
+    req.body.email,
+    req.body.role,
+    req.body.name
+  )
     .then(user =>
       res.status(200).json({
         success: true,
         payload: user
       })
     )
-  .catch(error =>
-    res.status(400).json({
-      success: false,
-      message: error.message
-    })
-  );
+    .catch(error =>
+      res.status(400).json({
+        success: false,
+        message: error.message
+      })
+    );
 
 export default handler;
