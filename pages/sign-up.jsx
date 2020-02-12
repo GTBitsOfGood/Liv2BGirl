33d72d3b5c92584590745aa59e27c133dc6db68c
Varshita Patakottu <varshita13@gmail.com>
2020-02-12 13:15:02 -0500

    added state

--- pages/sign-up.jsx
+++ pages/sign-up.jsx
@@ -4,27 +4,30 @@ import { Button } from 'reactstrap';
 
 
 const SignUp = () =>  {
-    const [email, password, invCode] = React.useState("");
+    const [email, setEmail, password, setPassword, invCode, setInvCode] = React.useState("");
+    // constructor(props) {
+    //     super(props)
+    //     this.state
+    // }
+    // changeEmail = event => {
+    //     email = event.target.value;
+    //     console.log(email);
+    // }
 
-    changeEmail = event => {
-        email = event.target.value;
-        console.log(email);
-    }
-
-    changePassword = event => {
-        password = event.target.value;
-    }
-    changeInvCode = event => {
-        invCode = event.target.value;
-    }
+    // changePassword = event => {
+    //     password = event.target.value;
+    // }
+    // changeInvCode = event => {
+    //     invCode = event.target.value;
+    // }
 
     return (
         <div>
             <Button style = {{WebkitTextFillColor: "#111111", backgroundColor:"lightGray"}} className = "logo" disabled> Logo </Button> {' '}
             <form> 
-                <input onClick={this.changeEmail} style = {{borderTop:0, borderLeft:0, borderRight:0}} className="form-control transparent-input" type = "text" placeholder = "Email"></input> <br></br>
-                <input onClick={this.changePassword} style = {{borderTop:0, borderLeft:0, borderRight:0}} className="form-control transparent-input" type = "text" placeholder = "Password"></input> <br></br>
-                <input onClick={this.changeInvCode} style = {{borderTop:0, borderLeft:0, borderRight:0}} className="form-control transparent-input" type = "text" placeholder = "Invitation Code"></input> <br></br>
+                <input onClick={(event) => { setEmail(event.target.value)}} style = {{borderTop:0, borderLeft:0, borderRight:0}} className="form-control transparent-input" type = "text" placeholder = "Email"></input> <br></br>
+                <input onClick={(event) => { setPassword(event.target.value)}} style = {{borderTop:0, borderLeft:0, borderRight:0}} className="form-control transparent-input" type = "text" placeholder = "Password"></input> <br></br>
+                <input onClick={(event) => { setInvCode(event.target.value)}} style = {{borderTop:0, borderLeft:0, borderRight:0}} className="form-control transparent-input" type = "text" placeholder = "Invitation Code"></input> <br></br>
             </form>
             <Button style = {{WebkitTextFillColor: "#111111"}} className = "button"> Sign Up </Button> {' '}
         </div>
