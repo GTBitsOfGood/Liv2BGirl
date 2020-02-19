42b483d74c0973aa0e3d92a515605d113be35423
Xingyun Chang <xchang31@gatech.edu>
2020-02-18 19:19:58 -0500

    Update followUser.js

--- pages/api/followUser.js
+++ pages/api/followUser.js
@@ -1,1 +1,21 @@
+import { follow } from "../../server/example/actions/User";
 
+// @route   POST api/follow
+// @desc    Follow Request
+// @access  Public
+const handler = (req, res) =>
+  follow(req.body.userId, req.body.username)
+    .then(user =>
+      res.status(200).json({
+        success: true,
+        payload: user
+      })
+    )
+    .catch(error =>
+      res.status(400).json({
+        success: false,
+        message: error.message
+      })
+    );
+
+export default handler;
