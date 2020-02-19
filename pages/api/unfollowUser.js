92cf4c4cb66ad313f11b628d8cafbd3e0361d919
Xingyun Chang <xchang31@gatech.edu>
2020-02-18 19:20:28 -0500

    Update unfollowUser.js

--- pages/api/unfollowUser.js
+++ pages/api/unfollowUser.js
@@ -1,1 +1,21 @@
+import { unfollow } from "../../server/example/actions/User";
 
+// @route   POST api/unfollow
+// @desc    Unfollow Request
+// @access  Public
+const handler = (req, res) =>
+  follow(req.body.userId, req.body.username)
+    .then(user =>
+      res.status(200).json({
+        success: true,
+        payload: user
+      })
+    )
+    .catch(error =>
+      res.status(400).json({
+        success: false,
+        message: error.message
+      })
+    );
+
+export default handler;
