5d8b02669d6c1a10a84746c45351575d86a99d14
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-02-22 16:09:45 -0500

    Re-add missing API request

--- client/actions/api.js
+++ client/actions/api.js
@@ -34,14 +34,36 @@ export const signUp = (username, password, email, role, name) =>
       username
     })
   })
-    .then(response => {
-      return response.json();
-    })
+    .then(response => response.json())
     .then(json => {
       if (json == null) {
         throw new Error("Could not connect to API!");
       } else if (!json.success) {
         throw new Error(json.message);
       }
+      return json.payload;
+    });
+
+export const login = (email, password) =>
+  fetch(urls.baseUrl + urls.api.login(), {
+    method: "POST",
+    mode: "same-origin",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email,
+      password
+    })
+  })
+    .then(response => response.json())
+    .then(json => {
+      if (json == null) {
+        throw new Error("Could not connect to API!");
+      } else if (!json.success) {
+        return json.message;
+      }
+
       return json.payload;
     });
