4895c7e2d7b7d8d73d979d43c0d0e1b598aeb1cb
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-02-22 16:59:10 -0500

    Fixed unused missing import error

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,6 @@
 import App from "next/app";
 import React from "react";
 import Head from "next/head";
-import Header from "../client/components/Header";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "../public/static/App.css";
 import "../public/static/SignUp.css";
