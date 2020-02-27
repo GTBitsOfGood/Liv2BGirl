8d5436f124894e9770906b278b14c4ca130a547c
Marcelo G. Costa <33551496+Marto37@users.noreply.github.com>
2020-02-26 20:21:38 -0500

    Marcelo/bottom_navbar (#55)
    * added NavBar component
    
    * NavBar fixes
    
    * fixed imports in _app.jsx
    
    * better button spacing on NavBar
    
    * even better spacing
    
    * best and final spacing of buttons

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,24 +1,31 @@
 import App from "next/app";
 import React from "react";
 import Head from "next/head";
+import NavBar from "../client/components/NavBar";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "../public/static/App.css";
 import "../public/static/SignUp.css";
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, router } = this.props;
 
     return (
       <>
         <Head>
           <title>Liv2BGirl</title>
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins&display=swap"
+            rel="stylesheet"
+          />
         </Head>
+
         <div className="App">
           <div className="Content">
             <Component {...pageProps} />
           </div>
         </div>
+        {!["/sign-up", "/sign-in"].includes(router.asPath) && <NavBar />}
       </>
     );
   }
