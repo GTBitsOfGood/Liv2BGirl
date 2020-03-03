dff90900154b7eaaba565771d34e8c4d62b3800a
Varshita Patakottu <varshita13@gmail.com>
2020-03-02 22:34:03 -0500

    finished generate username page

--- frontend/components/GenUsername.jsx
+++ frontend/components/GenUsername.jsx
@@ -2,14 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-
 const GenUsername = props => {
   const { userid } = props;
   const [describe, setDescribe] = React.useState("");
   const [like, setLike] = React.useState("");
   const [lucky, setLucky] = React.useState("");
-  let genUser = describe.substring(0, 2) + like.substring(0, 2) + lucky.substring(0, 2) + Math.floor(Math.random() * 10)
+  const [username, setUsername] = React.useState("");
+  function genUser() {
+    let randDescBeg = Math.floor(Math.random() * 2);
+    let randLikeBeg = Math.floor(Math.random() * 2);
+    let randLuckyBeg = Math.floor(Math.random() * 2);
+    let randDesc = Math.floor(Math.random() * (describe.length - 2)) + 2;
+    let randLike = Math.floor(Math.random() * (like.length - 2)) + 2;
+    let randLucky = Math.floor(Math.random() * (lucky.length - 2)) + 2;
+    let username = describe.substring(randDescBeg, randDesc) 
+      + like.substring(randLikeBeg, randLike) + lucky.substring(randLuckyBeg, randLucky) 
+      + Math.floor(Math.random() * 10)
+    return username;
+  }
+  
   const [generate, setGenerate] = React.useState(false);
+  const descriptions = ["", "Kind", "Cool", "Bubbly", "Neat", "Brave", "Social", "Eager",  "Giving", 
+  "Shy", "Friendly"];
+  const favThings = ["", "Dogs", "Biking", "Bowling", "Reading", "Sports", "Skating", "Cats",  "Movies", 
+  "Birds", "Running"];
+  const numbers = ["", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const usernames = [[genUser(), genUser(), genUser()], [genUser(),
+    genUser(), genUser()], [genUser(), genUser(), genUser()]]
+
 
 
 
@@ -19,29 +39,89 @@ const GenUsername = props => {
         <Form>
         <FormGroup>
             <Label>Describe yourself in one word: </Label>
-            <Input onClick={() => {setGenerate(false)}} onChange={(event) => { setDescribe(event.target.value)}} style={{height:35}} name="email"/>
+            <Input 
+              onClick={() => {setGenerate(false)}} 
+              onChange={(event) => { setDescribe(event.target.value)}} 
+              type="select"
+              style={{height:35}}>
+                {descriptions.map((word, i)=> {
+                  return <option> {word} </option>
+                })}
+              </Input>
+              
         </FormGroup>
         <FormGroup>
             <Label> One thing you like:</Label>
-            <Input onClick={() => {setGenerate(false)}} onChange={(event) => { setLike(event.target.value)}} style={{height:35}} name="password"/>
+            <Input 
+              onClick={() => {setGenerate(false)}} 
+              onChange={(event) => { setLike(event.target.value)}} 
+              type="select"
+              style={{height:35}}>
+                {favThings.map((word, i)=> {
+                  return <option> {word} </option>
+                })}
+              </Input>
         </FormGroup>
         <FormGroup>
             <Label>Your lucky number: </Label>
-            <Input onClick={() => {setGenerate(false)}} onChange={(event) => { setLucky(event.target.value)}} style={{height:35}} name="select">
+            <Input 
+            onClick={() => {setGenerate(false)}} 
+            onChange={(event) => { setLucky(event.target.value)}} 
+            style={{height:35}} 
+            type="select">
+              {numbers.map((word, i)=> {
+                  return <option> {word} </option>
+                })}
             </Input>
         </FormGroup>
         </Form>
-        <Button onClick={() => {setGenerate(true)}} style={{height:30, WebkitBorderRadius:40, color:'black', paddingBottom:28}}> Generate! </Button>
+        <Button onClick={() => {setGenerate(true)}} style={{outlineColor:'lightgray', 
+          backgroundColor:'lightgray', height:30, WebkitBorderRadius:40, color:'black',
+          paddingBottom:28}}>
+             Generate! 
+        </Button>
         <br></br>
         <Label style={{marginTop:10}}>Pick your username: </Label> 
         <br></br>
         {generate && 
-          <Button style={{WebkitBorderRadius: 30, color:'white'}}> {genUser} </Button>
+          usernames.map((name, i) => {
+            return <div style={{alignItems:'center'}}>
+                <Button 
+                onClick={(e) => {setUsername(name[0])}}
+                style={{marginRight:25, marginBottom:20, height:40, width:100,
+                 fontSize:11, WebkitBorderRadius: 30, color:'gray', backgroundColor:'white'}}> 
+                {name[0]} 
+                </Button>
+
+                <Button 
+                onClick={(e) => {setUsername(name[1])}}
+                style={{marginRight:25, marginBottom:20, height:40, width:100, 
+                fontSize:11, WebkitBorderRadius: 30, color:'gray', backgroundColor:'white'}}> 
+                {name[1]} 
+                </Button>
+
+                <Button 
+                onClick={(e) => {setUsername(name[2])}}
+                style={{marginBottom:20, height:40, width:100, fontSize:11,
+                WebkitBorderRadius: 30, color:'gray', backgroundColor:'white'}}> 
+                {name[2]} 
+                </Button> 
+                <br></br>
+              </div>
+          })
+
+          
         }
+        <br></br>
+        <Button style={{outlineColor:'lightgray', 
+          backgroundColor:'lightgray', marginLeft:130, height:30, 
+          WebkitBorderRadius:40, color:'black', paddingBottom:28}}> 
+          Next Step 
+        </Button>
     </div>
 
 
-
+    
 
 
     
