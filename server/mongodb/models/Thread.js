90819cc6bc72ed5aac9e98761766012f975e0de7
Josh McFarlin <32019743+Josh-McFarlin@users.noreply.github.com>
2020-03-05 21:10:30 -0500

    Follow Users and Thread Model (#61)
    * Update User.js
    
    * Update User.js
    
    * Update User.js
    
    * Update User.js
    
    * Update User.js
    
    * Create followUser.js
    
    * Create unfollowUser.js
    
    * Update followUser.js
    
    * Update unfollowUser.js
    
    * Update User.js
    
    * Update followUser.js
    
    * Update unfollowUser.js
    
    * Update followUser.js
    
    * Update unfollowUser.js
    
    * Create Thread.js
    
    * Update Thread.js
    
    Co-authored-by: Xingyun Chang <xchang31@gatech.edu>
    Co-authored-by: Christopher Jeffrey Obando <35117309+chrisjobando@users.noreply.github.com>

---
+++ server/mongodb/models/Thread.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const ThreadSchema = new Schema({
+  poster: {
+    type: String,
+    required: true,
+  },
+  group: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String],
+  },
+  content: {
+    type: String,
+  },
+  comments: {
+    type: [String],
+  },
+  pictures: {
+    type: [String],
+  },
+  reports: {
+    type: [String],
+  },
+  postedAt: {
+    type: Date,
+  }
+});
+
+export default mongoose.models.Thread || mongoose.model("Thread", ThreadSchema);
+
