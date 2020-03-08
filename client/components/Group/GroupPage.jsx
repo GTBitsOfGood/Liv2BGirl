5e6ca7128d93625d9af4a7777bafc55c43ab8ffe
Chris <chrisjobando@gmail.com>
2020-03-08 16:09:01 -0400

    Added dummy data

--- client/components/Group/GroupPage.jsx
+++ client/components/Group/GroupPage.jsx
@@ -12,6 +12,51 @@ import commentPlusOutline from "@iconify/icons-mdi/comment-plus-outline";
 import { Button, Dropdown, DropdownToggle, DropdownMenu } from "reactstrap";
 import GroupPost from "./GroupPost";
 
+const fakeThreads = [
+  {
+    title: "Test 1",
+    summary:
+      "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "CrazyPurpleFox",
+    comments: 12,
+  },
+  {
+    title: "Test 2",
+    summary:
+      "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "HappyBlueTurtle",
+    comments: 24,
+  },
+  {
+    title: "Test 3",
+    summary:
+      "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "SadRedElephant",
+    comments: 0,
+  },
+  {
+    title: "Test 4",
+    summary:
+      "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "GrossWhiteCat",
+    comments: 3,
+  },
+  {
+    title: "Test 5",
+    summary:
+      "5 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "AngryGrayLion",
+    comments: 189,
+  },
+  {
+    title: "Test 6",
+    summary:
+      "6 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "HappyGreenDog",
+    comments: 72,
+  },
+];
+
 const GroupPage = props => {
   const { groupid } = props;
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -66,11 +111,15 @@ const GroupPage = props => {
           </Dropdown>
           <Icon className="add-post" width="1.5rem" icon={commentPlusOutline} />
         </div>
-        <GroupPost />
-        <GroupPost />
-        <GroupPost />
-        <GroupPost />
-        <GroupPost />
+        {fakeThreads.map(thread => (
+          <GroupPost
+            key="Thread"
+            title={thread.title}
+            summary={thread.summary}
+            author={thread.username}
+            comments={thread.comments}
+          />
+        ))}
       </div>
     </div>
   );
--- client/components/Group/GroupPost.jsx
+++ client/components/Group/GroupPost.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // Styling
 import "./GroupPage.scss";
@@ -7,37 +8,38 @@ import "./GroupPage.scss";
 import { Icon } from "@iconify/react";
 import bxCommentDetail from "@iconify/icons-bx/bx-comment-detail";
 
-const GroupThread = () => {
+const GroupPost = props => {
+  const { title, summary, author, comments } = props;
+
   return (
     <div className="group-thread">
       <div style={{ display: "flex" }}>
-        <h1 className="thread-title">Thread Title</h1>
+        <h1 className="thread-title">{title}</h1>
         <h2 className="thread-time">5h</h2>
       </div>
 
-      <h2 className="thread-summary">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </h2>
+      <h2 className="thread-summary">{summary}</h2>
       <div className="thread-details">
         <img
           className="author-avatar"
           src="https://picsum.photos/50/50"
           alt="Group Avatar"
         />
-        <p className="thread-author">username</p>
+        <p className="thread-author">{author}</p>
         <div className="thread-comments">
           <Icon icon={bxCommentDetail} />
-          <p>100</p>
+          <p>{comments}</p>
         </div>
       </div>
     </div>
   );
 };
 
-export default GroupThread;
+GroupPost.propTypes = {
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  comments: PropTypes.number.isRequired,
+};
+
+export default GroupPost;
