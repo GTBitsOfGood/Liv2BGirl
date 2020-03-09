0de63615d044f1ed7a94f8fcb1fbc05eb8b5691d
Chris <chrisjobando@gmail.com>
2020-03-09 01:03:40 -0400

    Fixed navbar spacing

--- client/components/NavBar/NavBar.jsx
+++ client/components/NavBar/NavBar.jsx
@@ -3,27 +3,27 @@ import { Navbar, Nav, NavItem, NavLink, Button } from "reactstrap";
 import "./NavBar.scss";
 
 const NavBar = () => (
-  <Navbar className="navbar" color="light" light expand="md">
-    <Nav navbar className="nav">
-      <NavItem className="nav-item">
+  <Navbar color="light" light expand="md">
+    <Nav navbar>
+      <NavItem>
         <NavLink href="/" exact>
           <Button className="navbar-button" />
         </NavLink>
         <p>Home</p>
       </NavItem>
-      <NavItem className="nav-item">
+      <NavItem>
         <NavLink href="/groups">
           <Button className="navbar-button" />
         </NavLink>
         <p>Group</p>
       </NavItem>
-      <NavItem className="nav-item">
+      <NavItem>
         <NavLink href="/ask-me">
           <Button className="navbar-button" />
         </NavLink>
         <p>Ask Me</p>
       </NavItem>
-      <NavItem className="nav-item">
+      <NavItem>
         <NavLink href="/notifications">
           <Button className="navbar-button" />
         </NavLink>
