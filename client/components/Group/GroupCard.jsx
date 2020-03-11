0dd7aa66b5d243c1edaf15dfaa2f2c44ca6c93ad
Chris <chrisjobando@gmail.com>
2020-03-11 15:14:54 -0400

    Added border on hover, added link from profile to group page

--- client/components/Group/GroupCard.jsx
+++ client/components/Group/GroupCard.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "next/link";
+
+import { Button } from "reactstrap";
 
 const GroupCard = props => {
   const { title, description, image } = props;
 
   return (
-    <div className="group-thread-card">
+    <Button
+      tag={Link}
+      className="group-thread-card"
+      href={`/app/groups/${title}`}
+    >
       <img className="group-avatar" src={image} alt="Group Pic" />
       <div className="thread-info">
         <h1 className="thread-card-title">{title}</h1>
         <h2 className="thread-card-summary">{description}</h2>
       </div>
-    </div>
+    </Button>
   );
 };
 
