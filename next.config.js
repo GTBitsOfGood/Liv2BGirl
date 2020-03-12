8474be656c576d8d41427d5bca01bccc1ad51899
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-03-12 12:23:48 -0400

    Logging env vars for debugging

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const prod = process.env.NODE_ENV === "production";
 
 if (!prod) {
   dotEnv.config();
+} else {
+  // eslint-disable-next-line no-console
+  console.log("Using env vars: ", process.env);
 }
 
 module.exports = withSass(
