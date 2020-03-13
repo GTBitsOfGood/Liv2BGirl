da7c766b3756a71ff7f86cad7b099262af46b7f9
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-03-13 00:50:28 -0400

    Updated Next to latest version for built in CSS and SCSS support

--- next.config.js
+++ next.config.js
@@ -1,30 +1,15 @@
 const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const withCss = require("@zeit/next-css");
 const dotEnv = require("dotenv");
 
 const prod = process.env.NODE_ENV === "production";
 
 if (!prod) {
   dotEnv.config();
-} else {
-  // eslint-disable-next-line no-console
-  console.log("Using env vars: ", process.env);
 }
 
-module.exports = withSass(
-  withCss(
-    withImages({
-      env: {
-        MONGODB: process.env.MONGODB,
-        JWTSECRET: process.env.JWTSECRET,
-      },
-      build: {
-        env: {
-          MONGODB: process.env.MONGODB,
-          JWTSECRET: process.env.JWTSECRET,
-        },
-      },
-    })
-  )
-);
+module.exports = withImages({
+  env: {
+    MONGODB: process.env.MONGODB,
+    JWTSECRET: process.env.JWTSECRET,
+  },
+});
