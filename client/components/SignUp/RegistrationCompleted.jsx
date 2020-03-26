ec762e129d9fdaa98d6c309640d72b278f122610
Chris <chrisjobando@gmail.com>
2020-03-26 18:12:11 -0400

    Fixed registration completed page

--- client/components/SignUp/RegistrationCompleted.jsx
+++ client/components/SignUp/RegistrationCompleted.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import checkmark from "../../../public/img/checkmark.png";
 
+// Styling
+import styles from "./signup.module.scss";
+
 const RegistrationCompleted = () => (
-  <div className="page complete-pg">
-    <img className="complete-check" src={checkmark} alt="Checkmark" />
-    <h1 className="complete-text">Registration Completed</h1>
+  <div className={`Page ${styles.CompletePage}`}>
+    <img className={styles.CompleteCheck} src={checkmark} alt="Checkmark" />
+    <h2 className={styles.CompleteText}>Registration Completed</h2>
   </div>
 );
 
