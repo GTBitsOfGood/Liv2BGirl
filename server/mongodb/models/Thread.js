6d468f074ed77983d41cc55794c45f56929d55c3
Xingyun Chang <xchang31@gatech.edu>
2020-04-01 15:58:24 -0400

    Update Thread.js

--- server/mongodb/models/Thread.js
+++ server/mongodb/models/Thread.js
@@ -3,12 +3,17 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const ThreadSchema = new Schema({
-  poster: {
+  id: {
     type: String,
     required: true,
     index: true,
   },
-  group: {
+  posterId: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  groupId: {
     type: String,
     required: true,
     index: true,
@@ -31,5 +36,35 @@ const ThreadSchema = new Schema({
     default: Date.now,
   },
 });
+
+// const ThreadSchema = new Schema({
+//     poster: {
+//       type: String,
+//       required: true,
+//       index: true,
+//     },
+//     group: {
+//       type: String,
+//       required: true,
+//       index: true,
+//     },
+//     title: {
+//       type: String,
+//       required: true,
+//       text: true,
+//     },
+//     tags: {
+//       type: [String],
+//       text: true,
+//     },
+//     content: {
+//       type: String,
+//     },
+//     postedAt: {
+//       type: Date,
+//       required: true,
+//       default: Date.now,
+//     },
+//   });
 
 export default mongoose.models.Thread || mongoose.model("Thread", ThreadSchema);
