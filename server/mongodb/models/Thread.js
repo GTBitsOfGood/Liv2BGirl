fd1d078a89e907d275945634b44aa5e8fdbbff1b
Xingyun Chang <xchang31@gatech.edu>
2020-04-01 16:01:33 -0400

    Update Thread.js

--- server/mongodb/models/Thread.js
+++ server/mongodb/models/Thread.js
@@ -37,34 +37,4 @@ const ThreadSchema = new Schema({
   },
 });
 
-// const ThreadSchema = new Schema({
-//     poster: {
-//       type: String,
-//       required: true,
-//       index: true,
-//     },
-//     group: {
-//       type: String,
-//       required: true,
-//       index: true,
-//     },
-//     title: {
-//       type: String,
-//       required: true,
-//       text: true,
-//     },
-//     tags: {
-//       type: [String],
-//       text: true,
-//     },
-//     content: {
-//       type: String,
-//     },
-//     postedAt: {
-//       type: Date,
-//       required: true,
-//       default: Date.now,
-//     },
-//   });
-
 export default mongoose.models.Thread || mongoose.model("Thread", ThreadSchema);
