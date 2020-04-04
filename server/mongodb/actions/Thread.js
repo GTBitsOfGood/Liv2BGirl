5d78cf3451dd36d22927eec6e12550d94e66c4e3
Marto37 <mcosta33@gatech.edu>
2020-04-03 23:32:07 -0400

    fixed docs for filterThreads API

--- server/mongodb/actions/Thread.js
+++ server/mongodb/actions/Thread.js
@@ -17,15 +17,15 @@ export async function deleteThread(threadId) {
 // Currently just filtering by date, expects dates in format 'YYYY-MM-DD' or null
 export async function filterThreads(groupId, option, lowerBound, upperBound) {
   await mongoDB();
-  if (lowerBound == null || lowerBound == 'undefined') {
+  if (lowerBound == null || lowerBound == "undefined") {
     lowerBound = new Date("0001-01-01");
   }
-  if (upperBound == null || upperBound == 'undefined') {
+  if (upperBound == null || upperBound == "undefined") {
     upperBound = new Date();
   }
   return Thread.find({
     group: groupId,
-    postedAt: { $gte: new Date(lowerBound), $lte: new Date(upperBound) }
+    postedAt: { $gte: new Date(lowerBound), $lte: new Date(upperBound) },
   }).then(threads => {
     if (threads) {
       if (threads.length) {
