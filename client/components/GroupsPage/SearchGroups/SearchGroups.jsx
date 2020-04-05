5ef93e14c496bafe22f4d4222e1c5e12efa3dcd0
Josh <jmcfarlin6@gatech.edu>
2020-04-04 23:02:29 -0400

    Added create group button to group search page

--- client/components/GroupsPage/SearchGroups/SearchGroups.jsx
+++ client/components/GroupsPage/SearchGroups/SearchGroups.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import Link from "next/link";
 import Group from "./Group";
 import { useDebounce } from "../../../utils/hooks";
 import classes from "./SearchGroups.module.scss";
+import urls from "../../../../utils/urls";
 
 const SearchGroups = ({ searchTerm, likeableGroups }) => {
   const [groups, setGroups] = React.useState(likeableGroups);
 
-  React.useEffect(() => {
-    if (searchTerm.length === 0) {
-      setGroups(likeableGroups);
-    }
-  }, [searchTerm]);
-
   const debouncedTerm = useDebounce(searchTerm, 500);
   React.useEffect(() => {
+    if (debouncedTerm.length === 0) {
+      setGroups(likeableGroups);
+    }
+
     // Search for groups
   }, [debouncedTerm]);
 
@@ -26,6 +26,16 @@ const SearchGroups = ({ searchTerm, likeableGroups }) => {
         {groups.map(group => (
           <Group key={group.id} info={group} />
         ))}
+        {groups.length === 0 && (
+          <div className={classes.noGroupsContainer}>
+            <p className={classes.noGroupsText}>
+              {`There is no group about ${debouncedTerm.toLowerCase()} for now.`}
+            </p>
+            <Link href={urls.pages.app.newGroup}>
+              <p className={classes.noGroupsButton}>Create a new one?</p>
+            </Link>
+          </div>
+        )}
       </div>
     </>
   );
