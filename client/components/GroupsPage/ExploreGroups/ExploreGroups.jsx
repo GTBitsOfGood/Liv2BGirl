ade2f09f8858bdb15f835a5ca28158e7265b5988
Josh <jmcfarlin6@gatech.edu>
2020-04-04 20:13:11 -0400

    Explore Groups page content and loading data

--- client/components/GroupsPage/ExploreGroups/ExploreGroups.jsx
+++ client/components/GroupsPage/ExploreGroups/ExploreGroups.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import classes from "./ExploreGroups.module.scss";
+import loadingData from "./loadingData";
+import urls from "../../../../utils/urls";
 
-const ExploreGroups = () => (
-  <div>
-    <h1>Explore</h1>
-  </div>
-);
+const ExploreGroups = () => {
+  const [categories, setCategories] = React.useState(loadingData.categories);
+  const [groups, setGroups] = React.useState(loadingData.groups);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.categoryContainer}>
+        <h1 className={classes.categoryHeader}>Category</h1>
+
+        <div className={classes.categoryGroups}>
+          {categories.map(category => (
+            <div className={classes.categoryType}>
+              <div className={classes.categoryImage} />
+              <p className={classes.categoryTitle}>{category.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={classes.mayLikeContainer}>
+        <div className={classes.mayLikeHeader}>
+          <h1 className={classes.mayLikeHeaderText}>Groups you may like</h1>
+          <h1 className={classes.mayLikeHeaderText}>O</h1>
+        </div>
+
+        <div className={classes.mayLikeGroups}>
+          {groups.map(group => (
+            <div className={classes.mayLikeType}>
+              <div className={classes.mayLikeImage} />
+              <p className={classes.mayLikeTitle}>{group.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className={classes.notFoundText}>
+        {"Couldn't find what you want? "}
+        <a className={classes.createText} href={urls.pages.app.newgroup}>
+          Create a Group
+        </a>
+      </p>
+    </div>
+  );
+};
 
 export default ExploreGroups;
---
+++ client/components/GroupsPage/ExploreGroups/loadingData.js
@@ -0,0 +1,44 @@
+const loadingData = {
+  categories: [
+    {
+      title: "Category 1",
+    },
+    {
+      title: "Category 2",
+    },
+    {
+      title: "Category 3",
+    },
+    {
+      title: "Category 4",
+    },
+    {
+      title: "Category 5",
+    },
+    {
+      title: "Category 6",
+    },
+    {
+      title: "Category 7",
+    },
+    {
+      title: "Category 8",
+    },
+  ],
+  groups: [
+    {
+      title: "Group 1",
+    },
+    {
+      title: "Group 2",
+    },
+    {
+      title: "Group 3",
+    },
+    {
+      title: "Group 4",
+    },
+  ],
+};
+
+export default loadingData;
