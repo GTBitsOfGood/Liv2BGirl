6655549566f346cbf2ad01aae5bdbb30441f7307
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-06-25 17:37:24 -0400

    Updates for beta test and move to Azure

--- next.config.js
+++ next.config.js
@@ -3,30 +3,36 @@ const dotEnv = require("dotenv");
 
 const prod = process.env.NODE_ENV === "production";
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: !prod && process.env.ANALYZE === "true",
+});
+
 if (!prod) {
   dotEnv.config();
 }
 
-module.exports = withImages({
-  env: {
-    BASE_URL: process.env.BASE_URL,
-    MONGODB: process.env.MONGODB,
-    JWTSECRET: process.env.JWTSECRET,
-  },
-  build: {
+module.exports = withBundleAnalyzer(
+  withImages({
     env: {
       BASE_URL: process.env.BASE_URL,
       MONGODB: process.env.MONGODB,
       JWTSECRET: process.env.JWTSECRET,
     },
-  },
-  webpack: (config, { dev }) => {
-    if (dev) {
-      const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+    build: {
+      env: {
+        BASE_URL: process.env.BASE_URL,
+        MONGODB: process.env.MONGODB,
+        JWTSECRET: process.env.JWTSECRET,
+      },
+    },
+    webpack: (config, { dev }) => {
+      if (dev) {
+        const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
 
-      config.plugins.push(new CaseSensitivePathsPlugin());
-    }
+        config.plugins.push(new CaseSensitivePathsPlugin());
+      }
 
-    return config;
-  },
-});
+      return config;
+    },
+  })
+);
