4c2a0e2bcc5f67fe6e35545359690789c53702f6
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-06-25 21:50:07 -0400

    Attempt to fix issue with Azure builds failing by moving to Express server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const compression = require("compression");
+const next = require("next");
+const cookieParser = require("cookie-parser");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({
+  dev,
+});
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.use(compression());
+    server.use(cookieParser());
+
+    server.all("*", (req, res) => handle(req, res));
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
