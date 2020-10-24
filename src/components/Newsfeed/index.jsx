49c675a7f4685e2dea1c65f73cbb9b7e9d9cd29c
Sebastian Molina <colombiasm24@gmail.com>
2020-10-24 14:22:09 -0400

    Add testing functionality for newsfeed into IndexHeader

---
+++ src/components/Newsfeed/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { getApprovedPosts } from "../../../actions/Post";
+import {post} from "../../../actions/Post";
+
+const Newsfeed = ({ createdBy, createdAt, content }) => {
+    return (
+        <p>Hello!</p>
+    )
+    
+    
+
+}
+
+
+
