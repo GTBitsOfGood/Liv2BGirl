8856e4ffc34224188b0b9e4bfc052cf3446b866f
hdemusg <sumedhgarimella@gmail.com>
2020-11-02 18:12:55 -0500

    reverting to bring back approvepost

--- server/mongodb/models/Post.js
+++ server/mongodb/models/Post.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
-<<<<<<< HEAD
+
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
@@ -68,55 +68,3 @@ async function handleApprove(provDoc) {
 Post.statics.handleApprove = handleApprove;
 
 export default mongoose.models.Post || mongoose.model("Post", PostSchema);
-=======
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      index: true,
-      required: true,
-    }, 
-
-    createdAt: {
-        type: Date, 
-        required: true, 
-        default: Date.now, 
-    }, 
-
-    approved: {
-        type: Boolean, 
-        required: true, 
-        default: false, 
-    }, 
-
-    content: {
-        type: String, 
-        required: true, 
-    },
-}); 
-
- async function handleDelete(provDoc) {
-     const doc =
-       this.getQuery != null ? await this.model.findOne(this.getQuery()) : provDoc;
-  
-     if (doc != null) {
-       const id = doc._id;
-       await mongoose
-         .model()
-         .updateMany({ groups: id }, { $pull: { groups: id } });
-         await mongoose.model("GroupThread").deleteMany({ group: id });
-     }
-
-   }
-  
-   PostSchema.pre("remove", handleDelete);
-   PostSchema.pre("findOneAndDelete", handleDelete);
-   PostSchema.pre("findOneAndRemove", handleDelete);
-   PostSchema.pre("deleteOne", handleDelete);
-   PostSchema.pre("deleteMany", handleDelete);
-
-<<<<<<< HEAD
-export default mongoose.models.Post || mongoose.model("Post", PostSchema);
->>>>>>> parent of f11f232... further post updates
-=======
-export default mongoose.models.Post || mongoose.model("Post", PostSchema);
->>>>>>> parent of f11f232... further post updates
