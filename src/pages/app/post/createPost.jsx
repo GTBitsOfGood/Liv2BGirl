548e204727d3b35a6d16703ac7731f43d3fd2219
Eric M <matthews.eric02@gmail.com>
2020-11-09 00:53:54 -0500

    fixed urls for Post fxns. updated createPost page

--- src/pages/app/post/createPost.jsx
+++ src/pages/app/post/createPost.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { createPost } from "../../../actions/Post";
 
-var content = "";
-const getText = (event) => {
-  content = event.target.value;
-};
+const PostPage = (currentUser) => {
+  const [content, setContent] = React.useState("");
 
-const createPostHelper = () => {
-  console.log(content);
-  createPost(content);
-};
+  const getText = (event) => {
+    setContent(event.target.value);
+  };
 
-const PostPage = () => (
-  <>
-    <h1>Create a Post!</h1>
-    <body>
-      <h2>Insert the contents of what you would like to post here!</h2>
-      <input onChange={getText}></input>
-    </body>
-  </>
-);
+  const createPostHelper = (currentUser) => {
+    createPost(currentUser, content);
+  };
+
+  return (
+    <>
+      <h1>Create a Post!</h1>
+      <body>
+        <h2>Insert the contents of what you would like to post here!</h2>
+        <input onChange={getText} placeholder="Enter text here..."></input>
+        <button onClick={() => createPostHelper(currentUser)}>Post</button>
+      </body>
+    </>
+  );
+};
 
 PostPage.showTopNav = true;
 PostPage.showBottomNav = true;
